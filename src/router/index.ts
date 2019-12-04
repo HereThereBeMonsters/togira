e03f37d665b7f511a30251b702fee8a2fdbcb2ef
henry <pierre.henry@disy.net>
2019-12-04 17:10:36 -0400

    Change rule disallowing semicolons to make them compulaory and add them everywhere.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -17,11 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/config',
+    name: 'config',
+    component: () => import(/* webpackChunkName: "config" */ '../views/Config.vue')
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Config.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="configuration">
+    <h1>Configuration</h1>
+    <p>The configuration parameters are stored locally in the browser's local storage.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'config',
+  components: {}
+};
+</script>
