9b4cc2aa500c9dd80b3b011ff63c6e02919b50b0
henry <pierre.henry@disy.net>
2021-08-04 09:40:05 +0200

    Update Toggl API URL to new host api.track.toggl.com; Add optional logigng of proxied requests.

--- proxy-config/http-proxy-middleware-config.js
+++ proxy-config/http-proxy-middleware-config.js
@@ -3,6 +3,9 @@
  * It is used both by the Vue CLI server and the Togira own backend.
  */
 
+// If true, all requests will be logged to the console. Only set to true for debugging
+const LOG_REQUESTS = false;
+
 function jiraCustomRouter (req) {
   // Jira host must be specified as HTTP header in the request
   const jiraHost = req.headers['x-target-jira-host']
@@ -17,22 +20,32 @@ function customizeJiraRequest (proxyReq, req, res) {
   proxyReq.setHeader('user-agent', 'TogiraApp');
 }
 
+// log original request and proxied request info in the format // [GET] [200] "/api" -> "http://www.example.com/api"
+function logRequest (proxyRes, req) {
+  if(!LOG_REQUESTS) { return; }
+  console.log(
+    `[${req.method}] [${proxyRes.statusCode}] "${req.path}" -> "${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path}"`
+  );
+}
+
 module.exports = { // http-proxy-middleware reference: https://github.com/chimurai/http-proxy-middleware
-  // Toggl
+  // Toggl API
   '^/api/toggl': {
-    target: 'https://www.toggl.com',
-      ws: true,
-      changeOrigin: true,
-      pathRewrite: {
+    target: 'https://api.track.toggl.com',
+    ws: true,
+    changeOrigin: true,
+    pathRewrite: {
       '^/api/toggl': '/api/v8'
     },
+    onProxyRes: logRequest,
     logLevel: 'error'
   },
-  // Jira
+  // Jira API
   '^/api/jira': {
-    target: 'http://jira',
+      target: 'http://jira',
       router: jiraCustomRouter, // use Jira host specified in request
       onProxyReq: customizeJiraRequest,
+      onProxyRes: logRequest,
       ws: true,
       changeOrigin: true,
       pathRewrite: {
@@ -41,3 +54,4 @@ module.exports = { // http-proxy-middleware reference: https://github.com/chimur
     logLevel: 'error'
   }
 };
+
